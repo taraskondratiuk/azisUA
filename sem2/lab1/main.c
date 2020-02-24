525ba9d4050703d63e7bf4dcf91319635e56bbb0
Taras <kondratiuk.taras123@gmail.com>
2020-02-24 23:24:19 +0200

    1

--- sem2/lab1/main.c
+++ sem2/lab1/main.c
@@ -40,17 +40,22 @@ double avgSquare(double *l, double *r, int len) {
     return sqrt(sumOfSquares / len);
 }
 
-double *firstDerivativeArr(const double vals[], int len) {
+double *firstDerivative(const double *vals, int len) {
     double *res = (double *) malloc(len * sizeof(double));
     for (int i = 1; i < len - 1; ++i) {
         res[i] = (vals[i + 1] - vals[i - 1]) / (2 * DELTA_T);
     }
     res[0] = res[1];
     res[len - 1] = res[len - 2];
+
+    FILE *firstDerivative = fopen("../first_derivative.txt", "w+");
+    for (int j = 0; j < len; ++j) {
+        fprintf(firstDerivative, "%le\n", res[j]);
+    }
     return res;
 }
 
-double *secondDerivativeArr(const double vals[], int len) {
+double *secondDerivative(const double *vals, int len) {
     double *res = (double *) malloc(len * sizeof(double));
     for (int i = 2; i < len - 3; ++i) {
         res[i] = (vals[i - 2] - 2 * vals[i] + vals[i + 2]);
@@ -59,7 +64,48 @@ double *secondDerivativeArr(const double vals[], int len) {
     res[1] = res[2];
     res[len - 1] = res[len - 3];
     res[len - 2] = res[len - 3];
+
+    FILE *secondDerivative = fopen("../second_derivative.txt", "w+");
+    for (int j = 0; j < len; ++j) {
+        fprintf(secondDerivative, "%le\n", res[j]);
+    }
     return res;
+}
+
+double *recursiveFilter(const double vals[], int len, double a1, double a2, double b0, double b1, double b2) {
+    double *res = (double *) malloc(len * sizeof(double));
+    FILE *filter = fopen("../filter.txt", "w+");
+    int g = 3;
+    for (int i = 0; i < g; i++) {
+        res[i] = vals[i];
+        fprintf(filter, "%le\n", res[i]);
+    }
+    for (int i = g; i < len; i++) {
+        res[i] = -a1 * res[i - 1] - a2 * res[i - 2] + b0 * vals[i] + b1 * vals[i - 1] + b2 * vals[i - 2];
+        fprintf(filter, "%le\n", res[i]);
+    }
+    return res;
+}
+
+void printMaximums(const double vals[], int len) {
+    printf("\n");
+    for (int i = 1; i < len - 1; ++i) {
+        if (vals[i - 1] < vals[i] && vals[i + 1] < vals[i]) {
+            printf("local minimum %le at index %d\n", vals[i], i);
+        }
+    }
+}
+
+void printIntersectionsWithLevel(const double vals[], int len, double level) {
+    double T;
+    for (int i = 1; i < len - 1; i++) {
+        if (vals[i] == level) {
+            printf("%.11f - intersection with level %.11f\n", i * DELTA_T, level);
+        }
+        if ((vals[i] < level && vals[i + 1] > level) || (vals[i] > level && vals[i + 1] < level)) {
+            printf("%.11f - intersection with level %.11f\n", ((2 * i) + 1) * DELTA_T / 2, level);
+        }
+    }
 }
 
 int main() {
@@ -72,12 +118,29 @@ int main() {
         a = realloc(a, ++n * sizeof(double));
     }
     n--;
-//    for (int i = 0; i < n; ++i) {
     printf("%.11f\n", min(&a[0], &a[n - 1]));
     printf("%.11f\n", max(&a[0], &a[n - 1]));
     printf("%.11f\n", avg(&a[0], &a[n - 1], n));
     printf("%.11f\n", avgSquare(&a[0], &a[n - 1], n));
-//    }
+    double *firstDer = firstDerivative(&a[0], n);
+    double *secondDer = secondDerivative(&a[0], n);
+    double *recFilter = recursiveFilter(&a[0], n, -0.6559, 0.2487, 0.1482, 0.2964, 0.1482);
+
+    printf("\nFirst derivative:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%.11f ", firstDer[i]);
+    }
+    printf("\nSecond derivative:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%.11f ", secondDer[i]);
+    }
+    printf("\nRecursive filter:\n");
+    for (int i = 0; i < n; ++i) {
+        printf("%.11f ", recFilter[i]);
+    }
+    printf("\n");
+    printMaximums(&a[0], n);
+    double level = -0.0001114828;
+    printf("\n");
+    printIntersectionsWithLevel(&a[0], n, level);
 }
-
-
